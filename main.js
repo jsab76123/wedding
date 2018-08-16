d59349f57c197725b793668364f0ca97389fdadf
mini90196 <mini90196@gmail.com>
2018-08-16 21:57:03 +0800

    upload

---
+++ main.js
@@ -0,0 +1,198 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ "./src/$$_lazy_route_resource lazy recursive":
+/*!**********************************************************!*\
+  !*** ./src/$$_lazy_route_resource lazy namespace object ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(function() {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "./src/app/app.component.css":
+/*!***********************************!*\
+  !*** ./src/app/app.component.css ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n"
+
+/***/ }),
+
+/***/ "./src/app/app.component.html":
+/*!************************************!*\
+  !*** ./src/app/app.component.html ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<section id=\"snow\">\n\n</section>\n\n<nav class=\"navbar d-flex justify-content-center nav-bg .navbar-expand-sm\" style=\"width:100vw\">\n  <div>\n    <img src=\"./assets/banner.svg\" style=\"height: 2vh;\">\n    <div class=\"d-flex justify-content-center\">\n      <a class=\"nav-link\" href=\"#1\">首頁</a>\n      <a class=\"nav-link\" href=\"#2\">婚宴地點</a>\n      <a class=\"nav-link\" href=\"#3\">地圖位置</a>\n      <a class=\"nav-link\" href=\"#4\">留言區</a>\n    </div>\n    <img src=\"./assets/banner.svg\" style=\"height: 2vh;\">\n  </div>\n\n</nav>\n\n\n\n<div id=\"example\">\n\n  <div class=\"section section1\" anchors=\"1\">\n    <!-- <div class=\"qa\" style=\"width: 60%; height: 50%; background-color: #e4ddc1de; position: absolute ; top: 20%; left: 20%;border-radius: 12px;\">\n      <h4 style=\"text-align: center\">請填寫您的資料</h4>\n      <p>姓名：</p>\n    </div> -->\n    <div class=\"d-flex justify-content-center\" style=\"margin-bottom: 15%\">\n      <a name=\"\" id=\"\" class=\"btn\" href=\"https://docs.google.com/forms/d/1kolcvuNRskfZ_zLvK6hJHh-RkdAvr39AE0c-uvbRdEg/edit?usp=sharing\" role=\"button\" style=\"color: white ;background-color: #343a40ad;border: none;border-radius: inherit;\n      width: 200px\" target=\"_blank\" (click)=\"open()\">我要參加</a>\n    </div>\n\n\n\n    <!-- <div class=\"d-flex justify-content-center\" style=\"position:absolute; top:45%; left: 50%;\">\n      <img src=\"../assets/love.svg\" class=\"img-fluid \" alt=\"love\" style=\"max-width: 5vh; max-height: 5vh\">\n    </div> -->\n\n\n    <!-- <img width=\"100%\" height=\"110%\" src=\"/assets//DSC_9888.jpg\"> -->\n  </div>\n  <div class=\"section\" anchors=\"2\">\n\n    <!-- <div class=\"d-flex justify-content-center align-items-baseline\" style=\"width:100%; position: absolute;top: -8%;\n   z-index: -1;\">\n      <img style=\"width: 100%;\" src=\"../assets/bk-top.svg\" alt=\"bk-top\">\n    </div> -->\n\n\n\n    <div class=\"flower-top\">\n      <img src=\"./assets/bk-top.svg\" alt=\"\" style=\"position: absolute;\n      top: 0px; z-index: -1;\">\n    </div>\n\n    <div class=\"container-fluid\" style=\"background-color: #f5c8b3bf; \">\n\n      <div class=\"row\">\n\n        <div class=\"col-md-4 offset-md-4 \" style=\"text-align: center\">\n          <img src=\"./assets/title.svg\" style=\"width: 14vh\" alt=\"關於婚宴\">\n          <h2>關於婚宴</h2>\n        </div>\n      </div>\n      <div class=\"row\">\n\n\n        <div class=\"col-md-6 offset-md-4 justify-content-center\" style=\"text-align: left;\">\n          <p class=\"title\">什麼時候？</p>\n          <p class=\"answer\">2018/09/15 星期六 晚上 06:00</p>\n          <p class=\"title\">在哪裡？</p>\n          <p class=\"answer\">900屏東縣屏東市瑞光路三段208號 3F &nbsp;維多廳</p>\n          <p class=\"title\">如何停車？</p>\n          <p class=\"answer\">維多利亞婚宴會館停車場</p>\n          <p class=\"title\">如何搭車？</p>\n          <p class=\"answer\">屏東火車站搭車至維多利亞約10分鐘車程</p>\n        </div>\n      </div>\n\n\n\n\n\n    </div>\n    <div class=\"flower-bottom \">\n      <img src=\"./assets/bk-bottom.svg \" alt=\" \" style=\"position: absolute; bottom: 0px; z-index: -1 \">\n    </div>\n\n  </div>\n\n\n  <!-- <div class=\"d-flex align-content-between flex-wrap \">\n      <img width=\"100% \" height=\"100% \" src=\"../assets/bk-top.svg \" alt=\"bk-top \">\n    </div> -->\n\n  <div class=\"section \" anchors=\"3\">\n\n    <div class=\"flower-top\">\n      <img src=\"./assets/bk-top.svg\" alt=\"\" style=\"position: absolute;\n          top: 0px; z-index: -1;\">\n    </div>\n\n    <div class=\"container-fluid\" style=\"background-color: #f5c8b3bf\">\n\n      <div class=\"row\">\n        <div class=\"col-md-4 offset-md-4 \" style=\"text-align: center \">\n          <img src=\"./assets/title.svg\" style=\"width: 14vh\" alt=\"關於婚宴\">\n          <h2>位置地圖</h2>\n        </div>\n      </div>\n      <div class=\"row\">\n        <div class=\"col-md-8 offset-md-2\">\n          <div class=\"d-flex justify-content-center\" style=\" height: 60vh\">\n            <iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3680.977529768251!2d120.50145835116933!3d22.69188039091716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e17b7866bb31d%3A0x1975553d7202931b!2z57at5aSa5Yip5Lqe5a605pyD6aSo!5e0!3m2!1szh-TW!2stw!4v1534265048218 \"\n              frameborder=\"0 \" width=\"100%\" height=\"90%\" style=\"border:0; \" allowfullscreen></iframe>\n          </div>\n\n        </div>\n      </div>\n\n\n    </div>\n    <div class=\"flower-bottom \">\n      <img src=\"./assets/bk-bottom.svg \" alt=\" \" style=\"position: absolute; bottom: 0px;; z-index: -1 \">\n    </div>\n\n  </div>\n  <div class=\"section\" anchors=\"4\">\n    <div class=\"flower-top\">\n      <img src=\"./assets/bk-top.svg\" alt=\"\" style=\"position: absolute;\n              top: 0px; z-index: -1;\">\n    </div>\n\n    <div class=\"container-fluid\" style=\"background-color: #f5c8b3bf\">\n\n      <div class=\"row\">\n        <div class=\"col-md-4 offset-md-4 \" style=\"text-align: center\">\n          <img src=\"./assets/title.svg\" style=\"width: 14vh\" alt=\"關於婚宴\">\n          <h2>留言區</h2>\n        </div>\n      </div>\n      <div class=\"row\">\n        <div class=\"col-md-6 offset-md-3\">\n          <div class=\"d-flex justify-content-center\" style=\"height: 60vh\">\n            <div class=\"fb-comments\" data-href=\"https://jsab76123.github.io/wedding\" data-numposts=\"5\" data-width=\"100%\" style=\"min-width: 100%\"></div>\n          </div>\n        </div>\n      </div>\n\n\n    </div>\n    <div class=\"flower-bottom \">\n      <img src=\"./assets/bk-bottom.svg \" alt=\" \" style=\"position: absolute; bottom: 0px;; z-index: -1 \">\n    </div>\n  </div>\n</div>\n"
+
+/***/ }),
+
+/***/ "./src/app/app.component.ts":
+/*!**********************************!*\
+  !*** ./src/app/app.component.ts ***!
+  \**********************************/
+/*! exports provided: AppComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'weddings';
+    }
+    AppComponent.prototype.open = function () {
+    };
+    AppComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-root',
+            template: __webpack_require__(/*! ./app.component.html */ "./src/app/app.component.html"),
+            styles: [__webpack_require__(/*! ./app.component.css */ "./src/app/app.component.css")]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.module.ts":
+/*!*******************************!*\
+  !*** ./src/app/app.module.ts ***!
+  \*******************************/
+/*! exports provided: AppModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppModule", function() { return AppModule; });
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm5/platform-browser.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _ng_bootstrap_ng_bootstrap__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @ng-bootstrap/ng-bootstrap */ "./node_modules/@ng-bootstrap/ng-bootstrap/fesm5/ng-bootstrap.js");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            declarations: [
+                _app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"]
+            ],
+            imports: [_ng_bootstrap_ng_bootstrap__WEBPACK_IMPORTED_MODULE_2__["NgbModule"],
+                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"]
+            ],
+            providers: [],
+            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/environments/environment.ts":
+/*!*****************************************!*\
+  !*** ./src/environments/environment.ts ***!
+  \*****************************************/
+/*! exports provided: environment */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+var environment = {
+    production: false
+};
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+/***/ }),
+
+/***/ "./src/main.ts":
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser-dynamic */ "./node_modules/@angular/platform-browser-dynamic/fesm5/platform-browser-dynamic.js");
+/* harmony import */ var _app_app_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/app.module */ "./src/app/app.module.ts");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./environments/environment */ "./src/environments/environment.ts");
+
+
+
+
+if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].production) {
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["enableProdMode"])();
+}
+Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformBrowserDynamic"])().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])
+    .catch(function (err) { return console.log(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/*!***************************!*\
+  !*** multi ./src/main.ts ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /Users/jack_cai/angular/weddings/src/main.ts */"./src/main.ts");
+
+
+/***/ })
+
+},[[0,"runtime","vendor"]]]);
+//# sourceMappingURL=main.js.map
